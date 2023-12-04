f2acdfabf7f2a4c57688c1e091499124f9895ad7
LobbyBots <130771902+LobbyBots@users.noreply.github.com>
2023-12-04 16:56:27 +0100

    Updated website.py
    New commands list

--- website.py
+++ website.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return redirect("https://lobbybots.ga/commands", code=302)
+    return redirect("https://lobbybots.xyz/commands", code=302)
 
 print('\033[36m' + "Add the link thats at the top of the Client Commands window to https://uptimerobot.com/ to keep your bot online 24/7!" + '\033[0m')
 
